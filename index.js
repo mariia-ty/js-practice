bc6a64a6324f22a4bd11bb8352790ecb78218eaa
mariia-ty <tymoshenkom@icloud.com>
2024-08-10 11:55:43 -0400

    learned new commands

---
+++ index.js
@@ -0,0 +1,21 @@
+const userName = "Mariia"; //constant is well.. constant 
+console.log(userName);
+let userAge = 17; //let allows info to change 
+console.log(userAge);
+let userEmail = "tymoshenkom@icloud.com";
+console.log(userEmail);
+let userLogin = "mariia-ty25";
+console.log(userLogin);
+let userPassword = "asdfg45tyhj";
+console.log(userPassword);
+
+console.log(typeof userName);
+console.log(typeof userAge); 
+
+//alert(userName);
+//confirm("Is this your email?");
+// let newEmail = prompt("Input your email:");
+// console.log(newEmail);
+
+userAge = 18;
+console.log(userAge);

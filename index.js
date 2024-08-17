be377264f2cf5208ba4b87871b239381ca7f3ed0
mariia-ty <tymoshenkom@icloud.com>
2024-08-17 14:15:40 -0400

    lesson 2

--- index.js
+++ index.js
@@ -1,70 +1,35 @@
-// const userName = "Mariia"; //constant is well.. constant
-// console.log(userName);
-// let userAge = 17; //let allows info to change
-// console.log(userAge);
-// let userEmail = "tymoshenkom@icloud.com";
-// console.log(userEmail);
-// let userLogin = "mariia-ty25";
-// console.log(userLogin);
-// let userPassword = "asdfg45tyhj";
-// console.log(userPassword);
-// console.log(typeof userName);
-// console.log(typeof userAge);
-//alert(userName);
-//confirm("Is this your email?");
-// let newEmail = prompt("Input your email:");
-// console.log(newEmail);
-// userAge = 18;
-// console.log(userAge);
+//Lesson 2
+// const x = 11;
+// const y = 5;
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// let value = 5;
+// value = value + 10;
+// //value += 10;
+// console.log(value);
+// const x = 5;
+// const y = 10;
+// const z = 5;
+// console.log("x > y", x > y);
+// console.log("x < y", x < y);
+// console.log("x < z", x < z);
+// console.log("x === z", x === z);
+// const valueA = "5";
+// console.log(Number(valueA));
+// console.log(valueA);
+// const valueB = "Hi";
+// console.log(Number(valueB));
+// console.log(typeof Number(valueA));
+// console.log(typeof String(valueA));
 
-//task 1
-// let welcomeMessage = "Welcome to our website!";
-// alert(welcomeMessage);
-// confirm("Would you like to register?");
-// let userFirstName = prompt("Enter your user first name:");
-// console.log(userFirstName);
-// let userLastName = prompt("Enter your last name:");
-// console.log(userLastName);
-// let userAge = prompt("Enter your age:");
-// console.log(userAge);
-// let userNickName = prompt("Enter your chooses nick name for the website:");
-// console.log(userNickName);
-// let userEmail = prompt("Enter your email:");
-// console.log(userEmail);
-// let userPassword = prompt("Enter your password:");
-// console.log(userPassword);
+// console.log(Number.parseInt("5px"));
+// console.log(Number.parseFloat("5.5564px"));
 
-//task 2
-// let str = "Привіт";
-// console.log(typeof str);
-// let num = 123;
-// console.log(typeof num);
-// let flag = true;
-// console.log(typeof flag);
-// let txt = "true";
-// console.log(typeof txt);
-
-//task 3
-const boolTrue = true;
-console.log(typeof boolTrue);
-const boolFalse = false; 
-console.log(typeof boolFalse);
-const word = "revolving";
-console.log(typeof word);
-const greeting = "hello";
-console.log(typeof greeting);
-const day = 11;
-console.log(typeof day);
-
-let answer = true;
-console.log(typeof answer);
-let year = 2024;
-console.log(typeof year);
-let month = 8;
-console.log(typeof month);
-let action = "run";
-console.log(typeof action);
-let question = "How are you?";
-console.log(typeof question);
-
+const validNumber = Number("67");
+console.log(Number.isNaN(validNumber));
+const invalidNumber = Number("Hello");
+console.log(Number.isNaN(invalidNumber));
 

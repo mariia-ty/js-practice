e1f7fa820450f0a48afbda3c60511f4842651d97
mariia-ty <tymoshenkom@icloud.com>
2024-08-31 14:26:48 -0400

    lesson 4 added

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
-//Lesson 3
-// const guestName = "Mariia";
-// const guestLastName = "Ty";
-// const hotelName = "Palazio";
-// const roomNum = 4060;
-// const message = `Guest ${guestName} ${guestLastName} is stayint in ${hotelName} in room number ${roomNum}`;
-// console.log(message);
+//Lesson 4
+//and
+// const num = 20;
+// const result = num > 50 && num < 30;
+// console.log(result);
+//or
+// const num = 5;
+// const result = num < 10 || num > 30;
+// console.log(result);
+//not
+// console.log(true);
+// console.log(!true);
 
-let groupName = "FE_1y_25_02_09_23 Students";
-let numOfStudents = 9;
-let subject = "FrontEnd";
-let mood = "happy";
-let message = `Hello our group is called ${groupName} there are ${numOfStudents} students here, we are studying ${subject}. We feel ${mood} while learning more about it!`;
+// let cost = 0;
+// const subsciption = 'pro';
+// if (subsciption === 'pro') {
+//     cost = 100;
+// }
+// console.log(cost);
+
+// let cost = 0;
+// const subsciption = "free";
+// if (subsciption === "pro") {
+//   cost = 100;
+// }
+// else {
+//     cost = 0
+// }
+// console.log(cost);
+
+// let cost;
+// const subsciption = "premium";
+// if (subsciption === "free") {
+//   cost = 0;
+// }
+// else if (subsciption === "premium") {
+//     cost = 500;
+// } else {
+//   console.log("Invalid subscription type");
+// }
+// console.log(cost);
+
+// let type;
+// const age = prompt('How old are you?');
+// if (age >= 18){
+//     type = 'adult;'
+//     console.log('You are an adult');
+// } else {
+//     type = 'child';
+//     console.log('You are a child');
+// }
+
+// const age = 20;
+// const type = age >= 18 ? 'adult' : 'child';
+// console.log(type);
+//last one is shown 
+// console.log(1 && 4 && 7 && 'mango');
+//true takes priority 
+// console.log("name" || "" || null || 0 || undefined);

7f3c25e3a08d6089bfc5cf23173d6d0d3272bd9a
mariia-ty <tymoshenkom@icloud.com>
2024-10-19 13:34:30 -0400

    objects

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 //lesson 11
+//object 
+const club = {
+    //info 
+    name: "Tech Wiz",
+    meetingRm: 240,
+    numOfMembers: 15,
+    budget: 134.5, 
+    acceptingMembers: true,
+
+    //methods
+    showName() {
+        console.log(this.name);
+    },
+    updateMembers(value) {
+        this.numOfMembers = value;   
+    },
+};
+
+console.log(club); //this logs every info to console
+console.log(club.name);
+
+club.theme = "dark"; //adding to info
+console.log(club.theme);
+
+console.log(club);
+
+club.executives = ["Jack", "Lily", "Ian", "Rose"];
+console.log(club);
+
+club.showName();
+club.updateMembers(7);
+console.log(club.numOfMembers);
